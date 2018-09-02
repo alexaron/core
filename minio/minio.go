8ef43fc1c500d75ae41dafcf6fbcc7225b773522
alexaron <aron.alexandru@me.com>
2018-09-02 18:00:29 +0100

    minio update. add secure option

--- minio/minio.go
+++ minio/minio.go
@@ -4,16 +4,19 @@ import (
 	"fmt"
 	"html/template"
 	"log"
-//	"os"
+
+	//	"os"
 	"time"
+
 	"github.com/minio/minio-go"
 )
 
 type Info struct {
-	Endpoint 	string
-	Port 		int
-	AccessKey 	string
-	SecretKey	string
+	Endpoint  string
+	Port      int
+	AccessKey string
+	SecretKey string
+	Secure 	  bool
 }
 
 type ObjectHandlers struct {
@@ -21,12 +24,12 @@ type ObjectHandlers struct {
 }
 
 func (c Info) Connect() *ObjectHandlers {
-	
-	minioClient, err := minio.New(fmt.Sprintf("%v:%v", c.Endpoint, c.Port), c.AccessKey, c.SecretKey, false)
+
+	minioClient, err := minio.New(fmt.Sprintf("%v:%v", c.Endpoint, c.Port), c.AccessKey, c.SecretKey, c.Secure)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	
+
 	objectHandler := &ObjectHandlers{
 		minioClient: minioClient,
 	}
@@ -34,12 +37,10 @@ func (c Info) Connect() *ObjectHandlers {
 	return objectHandler
 }
 
-
-
 func (o *ObjectHandlers) Map(baseURI string) template.FuncMap {
 	f := make(template.FuncMap)
 
-	f["CDNCSS"] = func(bucket string,  filepath string,  media string) template.HTML {
+	f["CDNCSS"] = func(bucket string, filepath string, media string) template.HTML {
 
 		path, err := o.GetPresignedURLHandler(bucket, filepath)
 
@@ -51,13 +52,25 @@ func (o *ObjectHandlers) Map(baseURI string) template.FuncMap {
 		return template.HTML(fmt.Sprintf(`<link media="%v" rel="stylesheet" type="text/css" href="%v" />`, media, path))
 	}
 
-	f["CDNIMG"] = func(bucket string,  filepath string,  class string) template.HTML {
+	f["CDNJS"] = func(bucket string, filepath string) template.HTML {
+
+		path, err := o.GetPresignedURLHandler(bucket, filepath)
+
+		if err != nil {
+			log.Println("JS Error:", err)
+			return template.HTML("<!-- JS Error: " + path + " -->")
+		}
+
+		return template.HTML(fmt.Sprintf(`<script type="text/javascript" src="%v"></script>`, path))
+	}
+
+	f["CDNIMG"] = func(bucket string, filepath string, class string) template.HTML {
 
 		path, err := o.GetPresignedURLHandler(bucket, filepath)
 
 		if err != nil {
 			log.Println("CSS Error:", err)
-			return template.HTML("<!-- CSS Error: " + path + " -->")
+			return template.HTML(fmt.Sprintf(`<img class="%v" src="img/placeholder.jpg" />`, class))
 		}
 
 		return template.HTML(fmt.Sprintf(`<img class="%v" src="%v" />`, class, path))
@@ -67,18 +80,17 @@ func (o *ObjectHandlers) Map(baseURI string) template.FuncMap {
 }
 
 // GetPresignedURLHandler - generates presigned access URL for an object.
-func (o *ObjectHandlers) GetPresignedURLHandler(bucketName string, objectName string)  (string, error) {
+func (o *ObjectHandlers) GetPresignedURLHandler(bucketName string, objectName string) (string, error) {
 	// The object for which the presigned URL has to be generated is sent as a query
 	// parameter from the client.
 	var err error
 	if objectName == "" {
 		return "", err
 	}
+	
 	presignedURL, err := o.minioClient.PresignedGetObject(bucketName, objectName, 30*time.Second, nil)
 	if err != nil {
 		return "", err
 	}
 	return fmt.Sprintf("%v", presignedURL), err
 }
-
-
